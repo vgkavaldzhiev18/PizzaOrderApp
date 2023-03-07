6595ae16f5460eb01addb0f99ab29ed3413bfe46
vgkavaldzhiev18 <vgkavaldzhiev18@codingburgas.bg>
2023-03-07 09:30:00 +0200

    Add project to repository.

---
+++ src/main/java/com/example/pizzaorderapp/PizzaOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaOrderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pizzaorderapp/PizzaOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
